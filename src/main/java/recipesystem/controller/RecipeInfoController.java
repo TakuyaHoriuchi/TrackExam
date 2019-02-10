97ecb34aa0238370a143510e56069a4f4b2ac422
horiuchit71 <takuya.horiuchi@g.softbank.co.jp>
2019-02-10 20:07:12 +0900

    実装手順(TODO)を記載。gitignore追加

--- src/main/java/recipesystem/controller/RecipeInfoController.java
+++ src/main/java/recipesystem/controller/RecipeInfoController.java
@@ -6,7 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RecipeInfoController {
-
+  // TODO Domain層のInterfaceを用意・Controller層のテストケースを用意して最低限実装する.
+  // TODO Controller層の実装.
+  // TODO ITテストケース追加.
+  // TODO Infrastructure層のInterfaceを用意・Domain層のテストケースを用意して最低限実装する.
+  // TODO Domain層の実装.
+  // TODO Infrastructure層の実装及び(あれば)テストケース追加.
+  // TODO RuntimeExceptionMapper追加.
+  // TODO ログ追加.
+    
   /**
    * レシピを新規追加するメソッド.
    * @return 処理結果内容.
